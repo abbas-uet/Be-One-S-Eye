987bca13f7778729089b5753be83e0ae920322d6
Abbas Ali <54166807+abbas-uet@users.noreply.github.com>
2021-01-12 16:32:47 +0500

    60% work Done
    Text and Object Recognition Done

--- BeOnesEye/app/src/main/java/com/alitech/beoneseye/ExtractedText.java
+++ BeOnesEye/app/src/main/java/com/alitech/beoneseye/ExtractedText.java
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ExtractedText extends AppCompatActivity {
-
+    TextView ExtractedtextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extracted_text);
+        ExtractedtextView=(TextView)findViewById(R.id.extractedTextView);
         Intent extractIntent=this.getIntent();
         String textExtracted=extractIntent.getStringExtra("Text Extracted");
-
+        ExtractedtextView.setText(textExtracted);
     }
 
     @Override
--- BeOnesEye/app/src/main/java/com/alitech/beoneseye/camerGallery.java
+++ BeOnesEye/app/src/main/java/com/alitech/beoneseye/camerGallery.java
@@ -12,6 +12,7 @@ import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -23,20 +24,27 @@ import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 
 import java.io.IOException;
+import java.util.List;
 
 public class camerGallery extends AppCompatActivity {
 
     private TextView myAccount,logInSignUp;
-    Button cameraIntent,galleryIntent,getTextButton;
+    Button cameraIntent,galleryIntent,getTextButton,getlandMarkText,getObjectText;
     ImageView imageToTextImage;
     ImageButton imageButton;
     TextRecognizer recognizer;
+    ImageLabeler labeler;
     TextView aText;
     Uri fileUri;
     @Override
@@ -45,6 +53,8 @@ public class camerGallery extends AppCompatActivity {
         setContentView(R.layout.activity_camer_gallery);
         imageToTextImage=(ImageView)findViewById(R.id.imagetoText);
         getTextButton=(Button)findViewById(R.id.getTextButton);
+        getlandMarkText=(Button)findViewById(R.id.getLandMarkText);
+        getObjectText=(Button)findViewById(R.id.getObjectText);
         myAccount=(TextView)findViewById(R.id.myAccount);
         logInSignUp=(TextView)findViewById(R.id.loginSIgnUp);
         aText=(TextView)findViewById(R.id.textView);
@@ -124,6 +134,40 @@ public class camerGallery extends AppCompatActivity {
 
             }
         });
+        getlandMarkText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        getObjectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageLabeler labeler= ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+                InputImage image=null;
+                try {
+                    image = InputImage.fromFilePath(getApplicationContext(), fileUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                labeler.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                            @Override
+                            public void onSuccess(List<ImageLabel> labels) {
+                                for (ImageLabel label : labels) {
+                                    String text = label.getText();
+                                    float confidence = label.getConfidence();
+                                    int index = label.getIndex();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(camerGallery.this, e.toString(), Toast.LENGTH_SHORT).show();
+                            }});
+            }
+        });
 
     }
 
@@ -146,6 +190,8 @@ public class camerGallery extends AppCompatActivity {
         imageToTextImage.setVisibility(View.VISIBLE);
         imageToTextImage.setImageURI(imageUri);
         getTextButton.setVisibility(View.VISIBLE);
+        getlandMarkText.setVisibility(View.VISIBLE);
+        getObjectText.setVisibility(View.VISIBLE);
     }
 
     private void setInvisibility() {
